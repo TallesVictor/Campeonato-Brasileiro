fd11cf298ddb4c0d5ee35b0897046e7de092c60a
Talles Victor <tallesvictor2012@gmail.com>
2021-04-02 03:24:21 -0300

    Alterar para ter estrutura e dados necessários do banco de dados.

--- campeonato-brasileiro.sql
+++ campeonato-brasileiro.sql
@@ -1,5 +1,5 @@
 
-CREATE DATABASE`campeonato-brasileiro`;
+CREATE DATABASE `campeonato-brasileiro`;
 
 USE `campeonato-brasileiro`;
 
@@ -19,7 +19,6 @@ CREATE TABLE `failed_jobs` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-
 DROP TABLE IF EXISTS `migrations`;
 
 CREATE TABLE `migrations` (
@@ -29,6 +28,12 @@ CREATE TABLE `migrations` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+
+insert  into `migrations`(`id`,`migration`,`batch`) values 
+(1,'2019_08_19_000000_create_failed_jobs_table',1),
+(2,'2021_03_30_200148_create_times',1),
+(3,'2021_03_30_230144_create_rodadas',1),
+(4,'2021_03_31_221237_create_posicao_time',1);
 
 DROP TABLE IF EXISTS `posicao_time`;
 
@@ -51,6 +56,29 @@ CREATE TABLE `posicao_time` (
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
+insert  into `posicao_time`(`id`,`posicao`,`time_id`,`jogos`,`vitorias`,`empate`,`derrota`,`gols_pro`,`gols_contra`,`status`,`created_at`,`updated_at`) values 
+(1,1,1,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(2,2,2,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(3,3,3,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(4,4,4,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(5,5,5,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(6,6,6,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(7,7,7,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(8,8,8,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(9,9,9,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(10,10,10,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(11,11,11,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(12,12,12,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(13,13,13,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(14,14,14,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(15,15,15,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(16,16,16,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(17,17,17,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(18,18,18,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(19,19,19,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00'),
+(20,20,20,0,0,0,0,0,0,0,'2021-04-02 03:10:00','2021-04-02 03:10:00');
+
+
 DROP TABLE IF EXISTS `rodadas`;
 
 CREATE TABLE `rodadas` (
@@ -66,8 +94,7 @@ CREATE TABLE `rodadas` (
   KEY `rodadas_time_fora_foreign` (`time_fora`),
   CONSTRAINT `rodadas_time_casa_foreign` FOREIGN KEY (`time_casa`) REFERENCES `times` (`id`),
   CONSTRAINT `rodadas_time_fora_foreign` FOREIGN KEY (`time_fora`) REFERENCES `times` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=277 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 DROP TABLE IF EXISTS `times`;
 
@@ -79,3 +106,26 @@ CREATE TABLE `times` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+insert  into `times`(`id`,`name`,`logo`,`created_at`,`updated_at`) values 
+(1,'América-MG','img/America-MG.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(2,'Athletico-PR','img/Athletico-PR.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(3,'Atlético-GO','img/atletico-go.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(4,'Atlético-MG','img/atletico-mg.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(5,'Bahia','img/bahia.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(6,'Bragantino','img/rb-bragantino.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(7,'Ceará','img/ceara.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(8,'Chapecoense','img/chapecoense.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(9,'Corinthians','img/Corinthians.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(10,'Cuiabá','img/Cuiaba_EC.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(11,'Flamengo','img/Flamengo.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(12,'Fluminense','img/fluminense.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(13,'Fortaleza','img/fortaleza.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(14,'Grêmio','img/gremio.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(15,'Internacional','img/internacional.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(16,'Juventude','img/juventude.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(17,'Palmeiras','img/Palmeiras.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(18,'Santos','img/santos.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(19,'São Paulo','img/sao-paulo.png','2021-04-02 06:10:00','2021-04-02 06:10:00'),
+(20,'Sport','img/sport.png','2021-04-02 06:10:00','2021-04-02 06:10:00');
